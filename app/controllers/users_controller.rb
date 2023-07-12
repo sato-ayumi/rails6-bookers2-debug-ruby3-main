4ac35d0142e08903e7ef6a177081556628dd0c8f
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-07-12 09:20:10 +0000

    [Finish] application task 7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
       end
     end
     @books = @user.books
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
     @book = Book.new
   end
 
